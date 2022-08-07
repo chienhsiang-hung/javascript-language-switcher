506b9b93eec890204ab7d3ec160d4630964daff7
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2022-08-07 11:29:53 +0800

    Update include.js

--- src/js/include.js
+++ src/js/include.js
@@ -2,10 +2,9 @@ $(function(){
     var includes = $('[data-include]');
     $.each(includes, function(){
         
-        var file = '/javascript-language-switcher/src/html/' + $(this).data('include') + '.html';
+        var file = 'src/html/' + $(this).data('include') + '.html';
 
-        $.get(file, function(data){
-            $(this).replaceWith(data);
-        });
+        $(this).load(file);
+        $(`${this} > *`).unwrap();
     })
 });
